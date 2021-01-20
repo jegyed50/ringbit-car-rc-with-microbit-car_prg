0d6f9c07f9e3c6af13db9f6cb71a559ee22d0ac6
jegyed50 <26919968+jegyed50@users.noreply.github.com>
2021-01-20 16:54:42 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,21 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "x") {
+        xValue = value
+    } else {
+        yValue = value
+    }
+})
+let rightwheel = 0
+let leftwheel = 0
+let yValue = 0
+let xValue = 0
+RingbitCar.init_wheel(AnalogPin.P2, AnalogPin.P1)
+basic.showIcon(IconNames.Triangle)
+radio.setGroup(90)
+xValue = 0
+yValue = 0
+basic.forever(function () {
+    leftwheel = yValue + xValue
+    rightwheel = yValue - xValue
+    RingbitCar.freestyle(leftwheel, rightwheel)
+})
