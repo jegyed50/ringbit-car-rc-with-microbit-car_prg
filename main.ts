22e9f0654fc791bd4f86b02e450c5a197e6f64cf
jegyed50 <26919968+jegyed50@users.noreply.github.com>
2021-01-20 17:11:51 +0100

    main.blocks, main.ts frissítése

--- main.ts
+++ main.ts
@@ -9,7 +9,7 @@ let rightwheel = 0
 let leftwheel = 0
 let yValue = 0
 let xValue = 0
-RingbitCar.init_wheel(AnalogPin.P2, AnalogPin.P1)
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
 basic.showIcon(IconNames.Triangle)
 radio.setGroup(90)
 xValue = 0
